64956d760cc523e3ea0dfd14612ae21e14973dcb
Chambi-Dev <a3820143@gmail.com>
2026-05-06 22:58:28 -0500

    feat: add Prisma and Usuarios modules, enable CORS and validation pipes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    PrismaModule,
+    UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
